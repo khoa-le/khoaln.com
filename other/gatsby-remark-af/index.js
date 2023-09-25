547a5ccffbf46711971723d9c2e8db47aa34ae3b
Khoa Le <le.ngoc.khoa@oro.com>
2023-09-25 15:22:31 +0700

    add blog

--- other/gatsby-remark-af/index.js
+++ other/gatsby-remark-af/index.js
@@ -1,24 +1,7 @@
 // gatsby remark affiliate link maker
-const {URL} = require('url')
-const visit = require('unist-util-visit')
+// const {URL} = require('url')
+// const visit = require('unist-util-visit')
 
-function affiliateLinkTransformer({markdownAST}) {
-  visit(markdownAST, 'link', linkNode => {
-    if (linkNode.url.includes('amazon.com')) {
-      const amazonUrl = new URL(linkNode.url)
-      if (!amazonUrl.searchParams.has('tag')) {
-        amazonUrl.searchParams.set('tag', 'kentcdodds-20')
-        linkNode.url = amazonUrl.toString()
-      }
-    }
-    if (linkNode.url.includes('egghead.io')) {
-      const eggheadUrl = new URL(linkNode.url)
-      if (!eggheadUrl.searchParams.has('af')) {
-        eggheadUrl.searchParams.set('af', '5236ad')
-        linkNode.url = eggheadUrl.toString()
-      }
-    }
-  })
-}
+function affiliateLinkTransformer() {}
 
 module.exports = affiliateLinkTransformer
